5e4ab1335565da5e520cea59d3f2941aa47840b2
tossyKim <dydrkfk22@gmail.com>
2025-03-12 11:43:47 +0900

    연습페이지

--- src/main/java/kr/ac/kopo/kyg/bookmarket/BookMarketApplication.java
+++ src/main/java/kr/ac/kopo/kyg/bookmarket/BookMarketApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookMarketApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BookMarketApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BookMarketApplication.class, args);
+	}
 
 }
