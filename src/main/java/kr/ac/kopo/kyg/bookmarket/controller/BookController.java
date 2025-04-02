1af06cb134c259b957c5d4e3351c0c49e40c0d37
tossyKim <dydrkfk22@gmail.com>
2025-04-02 11:42:58 +0900

    chapter 4 bookmarketcontroller 여러 방법으로 수정

--- src/main/java/kr/ac/kopo/kyg/bookmarket/controller/BookController.java
+++ src/main/java/kr/ac/kopo/kyg/bookmarket/controller/BookController.java
@@ -7,19 +7,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Controller
+@RequestMapping(value = "/books")
 public class BookController {
 
     @Autowired
     private BookService bookService;
 
-    @RequestMapping(value = "/books", method = RequestMethod.GET)
-    public String requestBookList(Model model) {
+    @RequestMapping
+    public String requestAllBookList(Model model) {
         List<Book> bookList = bookService.getAllBookList();
         model.addAttribute("bookList", bookList);
         return "books";
+    }
+    @RequestMapping("/all")
+    public ModelAndView requestAllBookList1(Model model) {
+        ModelAndView modelV = new ModelAndView();
+        modelV.setViewName("books");
+        List<Book> bookList = bookService.getAllBookList();
+        modelV.addObject("bookList", bookList);
+        return modelV;
     }
 }
